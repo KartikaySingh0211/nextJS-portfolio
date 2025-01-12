616dcf54485cee0905807e043a8cd6f51a91aa95
Kartikay Singh <kartikay.singh.0211@gmail.com>
2025-01-12 11:49:24 +0530

    First commit

---
+++ data/index.ts
@@ -0,0 +1,160 @@
+export const navItems = [
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Experience", link: "#experience" },
+	{ name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: "I prioritize collaboration, fostering open communication ",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/b1.svg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	// {
+	// 	id: 5,
+	// 	title: "Currently building a JS Animation library",
+	// 	description: "The Inside Scoop",
+	// 	className: "md:col-span-3 md:row-span-2",
+	// 	imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+	// 	titleClassName: "justify-center md:justify-start lg:justify-center",
+	// 	img: "/b5.svg",
+	// 	spareImg: "/grid.svg",
+	// },
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "Animated Apple Iphone 3D Website",
+		des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+		img: "/p1.svg",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+		link: "https://github.com/KartikaySingh0211/AppleWebsiteUI_clone",
+	},
+	{
+		id: 2,
+		title: "Three.js Portfolio Website",
+		des: "Developed a portfolio website using Three.js, React, and Tailwindcss, showcasing my projects and skills.",
+		img: "/p2.png",
+		iconLists: ["/re.svg", "/tail.svg", "/three.svg"],
+		link: "https://github.com/KartikaySingh0211/Portfolio",
+	},
+	// {
+	// 	id: 3,
+	// 	title: "This portfolio website",
+	// 	des: "Designed and developed my personal portfolio website using Next.js, Tailwindcss, TypeScript and AceternityUI .",
+	// 	img: "/p3.svg",
+	// 	iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/acet.svg"],
+
+	// },
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: "cloudinary",
+		img: "/cloud.svg",
+		nameImg: "/cloudName.svg",
+	},
+	{
+		id: 2,
+		name: "appwrite",
+		img: "/app.svg",
+		nameImg: "/appName.svg",
+	},
+	{
+		id: 3,
+		name: "HOSTINGER",
+		img: "/host.svg",
+		nameImg: "/hostName.svg",
+	},
+	{
+		id: 4,
+		name: "stream",
+		img: "/s.svg",
+		nameImg: "/streamName.svg",
+	},
+	{
+		id: 5,
+		name: "docker.",
+		img: "/dock.svg",
+		nameImg: "/dockerName.svg",
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: "Frontend Engineer Intern",
+		desc: "Assisted in the development of a web-based platform using Next.js, enhancing interactivity.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	{
+		id: 2,
+		title: "Lead Frontend Developer",
+		desc: "Developed and maintained user-facing features using modern frontend technologies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp4.svg",
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/git.svg",
+		link: "https://github.com/KartikaySingh0211",
+	},
+	{
+		id: 2,
+		img: "/link.svg",
+		link: "www.linkedin.com/in/kartikaysingh0211",
+	},
+];
