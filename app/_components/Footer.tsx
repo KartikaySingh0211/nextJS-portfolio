30d5d0a180ac8a81d54158582535917b0f4e4b90
Kartikay Singh <kartikay.singh.0211@gmail.com>
2025-01-13 11:54:23 +0530

    .

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
 					Reach out to me today and let&apos;s discuss how I can help you
 					achieve your goals.
 				</p>
-				<a href="mailto:kartikay.singh.0211@gmail.com">
+				<a href="mailto:kartikay.singh.0211@gmail.com" title="Email">
 					<MagicButton
 						title="Let's get in touch"
 						icon={<FaLocationArrow />}
@@ -36,6 +36,7 @@ const Footer = () => {
 							key={info.id}
 							href={info.link}
 							target={"_blank"}
+							title="Social Media"
 							className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
 						>
 							<Image src={info.img} alt="icons" width={20} height={20} />
